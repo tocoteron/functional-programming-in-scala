0eaa3a246f1c01cd26c2d9ee4486d7444548965d
tocoteron <tocoteron@gmail.com>
2022-02-06 22:09:28 +0900

    Fix the block

--- src/test/scala/TestExercise2_1.scala
+++ src/test/scala/TestExercise2_1.scala
@@ -3,7 +3,8 @@ import org.junit.Assert.*
 import Exercise2_1._
 
 class TestExercise2_1:
-  @Test def testFib(): Unit =
+  @Test def testFib(): Unit = {
     assertEquals(0, fib(0))
     assertEquals(1, fib(1))
     assertEquals(55, fib(10))
+  }
